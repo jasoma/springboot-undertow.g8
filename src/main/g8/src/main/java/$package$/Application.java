39c4ebe9ada3eb4c36629516345d5ad5ecb6373a
Jason Maher <jason@jasonmaher.me>
2015-10-27 12:01:56 +0900

    Basic template, main Application class and sample controller

---
+++ src/main/g8/src/main/java/$package$/Application.java
@@ -0,0 +1,18 @@
+package $package$;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main configuration class for the SpringBoot application.
+ */
+@SpringBootApplication
+public class Application {
+
+    /**
+     * Entry point.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
